97a67710ecddea3e51ca4d22b6964a85965d4d4b
m1lene <141060425+m1lene@users.noreply.github.com>
2024-06-15 17:29:53 -0300

    up

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,29 @@ const changeThemeBtn = document.querySelector("#change-theme")
 
 changeThemeBtn.addEventListener("change", function(){
     document.body.classList.toggle("dark");
-    
-
 })
+
+
+$(function(){
+    abrirJanela();
+    verificarCliqueFechar();
+
+    function abrirJanela(){
+        $('.btn').click(function(e){
+            e.stopImmediatePropagation();
+            $('.bg').fadeIn();
+        });
+    }
+
+    function verificarCliqueFechar(){
+        var el = $('body,.closeBtn');
+
+        el.click(function(){
+            $('.bg').fadeOut();
+        })
+        
+        $('.form').click(function(e){
+            e.stopImmediatePropagation()
+        })
+    }
+});
